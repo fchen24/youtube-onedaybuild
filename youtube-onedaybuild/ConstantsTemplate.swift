879a5ab0253d68fdb5b625d893c24fcce59aea2e
fei.chen <fchen24@outlook.com>
2020-10-08 23:19:33 +0800

    modified .gitignore to stop tracking Constants.swift, added ConstantsTemplate.swift instead

---
+++ youtube-onedaybuild/ConstantsTemplate.swift
@@ -0,0 +1,21 @@
+//
+//  ConstantsTemplate.swift
+//  youtube-onedaybuild
+//
+//  Created by 陳飛 on 8/10/20.
+//
+
+/*
+Add your own keys below and uncomment the code
+ */
+
+import Foundation
+
+/*
+struct Constants {
+    
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
+    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=5&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
+*/
---
+++ youtube-onedaybuild/Video.swift
@@ -0,0 +1,18 @@
+//
+//  Video.swift
+//  youtube-onedaybuild
+//
+//  Created by 陳飛 on 8/10/20.
+//
+
+import Foundation
+
+
+struct Video {
+    
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var publishedAt = ""
+}
